bf062be95d85d34ccedf383870a9643d7fce8757
Beidysy <beydisy@gmail.com>
2025-03-22 04:02:34 -0400

    Fixed the doctor service for changing database user - after unit testing

--- doctor-service/app/config.py
+++ doctor-service/app/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@postgres/doctor_db"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://root:root@postgres/doctor_db"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://group3:group3pass@postgres:5432/doctor_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://group3:group3pass@localhost:5432/doctor_db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
