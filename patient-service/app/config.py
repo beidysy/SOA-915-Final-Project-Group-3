5fb142b8211949a175545df55a80c00b795030ec
Beidysy <beydisy@gmail.com>
2025-03-22 05:36:14 -0400

    Patient service fixed, after unit texting

--- patient-service/app/config.py
+++ patient-service/app/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@postgres/patient_db"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres/patient_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 👇 Add this line inside the class to print
+    print("🧠 Using DB URI:", SQLALCHEMY_DATABASE_URI)
 
