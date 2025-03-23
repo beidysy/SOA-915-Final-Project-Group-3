9f8ded57b95d8886081d9365e0fe74cf0a96d253
Beidysy <beydisy@gmail.com>
2025-03-23 03:02:46 -0400

    Appointment service Fixedgit add .!

--- appointment-service/app/config.py
+++ appointment-service/app/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@postgres/appointment_db"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres/appointment_db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
